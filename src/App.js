8ecaf48992f5558663109ad2ba809019ae7989fe
Shari Nielsen <sharinielsen@web.de>
2021-04-09 12:02:49 +0200

    created a header function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import { BrowserRouter  as Router } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Harry Potter</div>;
+  return (
+    <Router> 
+    <div className="App">
+      <header> 
+        <Header/>
+      </header>
+    Harry Potter
+    </div>;
+    </Router>
+  )
 }
 
 export default App;
