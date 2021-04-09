f47e51875b98877d25963709babe2798e4a53357
Shari Nielsen <sharinielsen@web.de>
2021-04-09 11:44:22 +0200

    Delete not needed files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import { BrowserRouter  as Router } from "react-router-dom";
 
 function App() {
-  return <div className="App">Rick & Morty</div>;
+  return <div className="App">Harry Potter</div>;
 }
 
 export default App;
