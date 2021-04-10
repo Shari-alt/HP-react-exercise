e1d30579ccdb72981e520827c15593a05bb5e8ad
Shari Nielsen <sharinielsen@web.de>
2021-04-10 19:20:48 +0200

    Switched to RickandMorty API

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import ExpectoPatronum from "..image/ExpectoPatronum.svg";
 export default function Home () {
     return (
         <div>
-            <h1> Welcome Muggle</h1>
+            <h1> Welcome to Rick & Morty</h1>
         </div>
     )
 }
