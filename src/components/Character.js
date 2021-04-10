e853d2750a9fdf5bccbb458a9cacf6836372af88
Shari Nielsen <sharinielsen@web.de>
2021-04-10 20:45:02 +0200

    Added Character function

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,29 +2,54 @@ import useState, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Characterlist () {
-    const [characters, setCharacters]=useState();
+   const [page, setPage] = useState();
+   const[totalPages, setTotalPages] = useState();
+   const [characters, setCharacters]=useState([]); /* Why the '[]'?*/ 
 
-    useEffect(() => {
-        const url = `http://hp-api.herokuapp.com/api/characters`;
-        fetch (url) 
-        .then ((res) => res.json())
-        .then((data) => {
-        setCharacters(data);
+  useEffect(() => {
+   const url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+   
+   fetch(url)
+     .then((res) => res.json())
+     .then((dataFromApi) => {
+         setCharacters((prevUsers) => {
+             return [...prevUsers, ...dataFromApi.results];
+            });
+            setTotalPages(dataFromApi.pages);
+        });
+}, [page]);
+
+function renderCharacters () {
+    return characters.map((character) => {
+        const { name, id, image} = character;
+
+        return (
+            <li key={id}>
+                <Link to={`/character/${id}`}>
+                    {name} {image}
+                </Link>
+            </li>
+        );
     });
- }, []);
+}
 
-    function renderCharacter () {
-        const characterList = characters.map((characters) => (
-           <SingleCharacter
-           key={characters.id}
-           name={characters.name}
-           species={characters.species}
-           house={characters.house}
-           patronus={characters.patronus}
-           image={characters.image}
-           />
-        ));
-        return characterList;
+function handleLoadMore(){
+    if (page < totalPages){
+        setPage(page +1);
     }
+}
+
+return (
+    <div>
+        <h2> This is the wonderful world of Rick and Morty</h2>
+        <ul> {renderCharacters} </ul>
+        {page <totalPages && (
+            <button onClick={handleLoadMore}> Want to see more? </button>
+        )}
+    </div>
+);
+}
+
+
     
    
--- src/components/SingleCharacter.js
+++ src/components/SingleCharacter.js
@@ -1,4 +1,31 @@
-export default function SingleCharacter ({name, patronus, house, species, image}) {
+// export default function SingleCharacter ({name, patronus, house, species, image}) {
+//     return (
+//         <section className="SingleUser">
+//             <h2> {name} </h2>
+//             <p> {patronus}</p>
+//             <p> {house}</p>
+//             <p> {species}</p>
+//             <img src={image} alt={name} />
+//         </section>
+
+//     )
+// }
+
+// function SingleCharacter ({name, patronus, house, species, image}) {
+function SingleCharacter () {
+    const {id} = useParams();
+    const [character, setCharacter] = useState({}) 
+    
+    useEffect(() => {
+        const url = `http://hp-api.herokuapp.com/api/characters/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data)=> {
+        setCharacter(data)
+    })
+
+    }, [id]);
+    
     return (
         <section className="SingleUser">
             <h2> {name} </h2>
