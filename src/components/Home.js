cb51b4c1bf2ee85bf05cd7a3940987222da4fb7f
Shari Nielsen <sharinielsen@web.de>
2021-04-10 19:27:57 +0200

    Finished Home function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
-import ExpectoPatronum from "..image/ExpectoPatronum.svg";
 export default function Home () {
     return (
         <div>
-            <h1> Welcome to Rick & Morty</h1>
+            <h1> Welcome to Rick and Morty</h1>
         </div>
     )
 }
